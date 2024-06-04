90e1feafc611d3cafef3cff69db8e9877f32d1b0
Alexander Rodriguez <160079823+AXrodriguezQ@users.noreply.github.com>
2024-06-04 14:24:40 +0200

    add connection of db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import config from './config/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
-    })
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: config().database.host,
+      port: config().database.port,
+      username: config().database.username,
+      password: config().database.password,
+      database: config().database.db,
+      autoLoadEntities: true,
+      synchronize: true
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/config/config.ts
@@ -0,0 +1,10 @@
+
+export default () => ({
+    database: {
+        host : process.env.DATABASE_HOST,
+        port : parseInt(process.env.DATABASE_PORT),
+        username : process.env.DATABASE_USERNAME,
+        password : process.env.DATABASE_PASSWORD,
+        db : 'postgres'
+    }
+})
